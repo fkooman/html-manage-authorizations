4764e579872ef6e10bfedb1fe45209aaf3da13ca
François Kooman <fkooman@tuxed.net>
2012-08-02 11:56:36 +0200

    update install_dependencies script to work on Mac, add utf-8 charset, cleanup text

--- docs/install_dependencies.sh
+++ docs/install_dependencies.sh
@@ -4,10 +4,10 @@ rm -rf ext
 mkdir -p ext/js
 
 # jQuery
-wget -O ext/js/jquery.js http://code.jquery.com/jquery.min.js
+curl -o ext/js/jquery.js http://code.jquery.com/jquery.min.js
 
 # JSrender (JavaScript Template Rendering for jQuery)
-wget -O ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsrender.js
+curl -o ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsrender.js
 
 # JSO (JavaScript OAuth 2 client)
-wget -O ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
+curl -o ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
