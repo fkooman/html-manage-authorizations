af6509c0560398a8cb359cc7583bfec262db0d90
F. Kooman <fkooman@tuxed.net>
2012-09-08 19:20:54 +0200

    use bootstrap for styling

--- docs/install_dependencies.sh
+++ docs/install_dependencies.sh
@@ -2,6 +2,7 @@
 
 rm -rf ext
 mkdir -p ext/js
+mkdir -p ext/bootstrap
 
 # jQuery
 curl -o ext/js/jquery.js http://code.jquery.com/jquery.min.js
@@ -11,3 +12,9 @@ curl -o ext/js/jsrender.js https://raw.github.com/BorisMoore/jsrender/master/jsr
 
 # JSO (JavaScript OAuth 2 client)
 curl -o ext/js/jso.js https://raw.github.com/andreassolberg/jso/master/jso.js
+
+# Bootstrap
+curl -o ext/bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
+(cd ext/ && unzip bootstrap.zip && rm bootstrap.zip)
+
+
